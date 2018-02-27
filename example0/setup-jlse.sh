3b6c45eb70fdfee0d59e290bccb50dec19fa0880
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-02-27 21:29:21 +0000

    Convenience script

---
+++ example0/setup-jlse.sh
@@ -0,0 +1,8 @@
+
+# SETUP JLSE
+
+# Source this
+
+PATH=$HOME/Public/sfw/icc/swift-t/stc/bin:$PATH
+PATH=$HOME/Public/sfw/icc/swift-t/turbine/bin:$PATH
+export LD_LIBRARY_PATH=/soft/compilers/intel/compilers_and_libraries_2017.2.174/linux/compiler/lib/intel64_lin
